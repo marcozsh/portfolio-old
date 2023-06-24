bf541e9e05dd1a5c4d9aa04a5064d0e691a8c9aa
marcozsh <marc.penar@outlook.cl>
2023-06-24 03:18:13 -0400

    initial-commit

---
+++ JS/index.js
@@ -0,0 +1,47 @@
+window.onbeforeunload = ()=>{
+    window.scrollTo(0,0);
+}
+// mouse effect
+const main = document.querySelector(".body");
+const mouse_over = document.querySelector(".mouse-over");
+main.addEventListener('mousemove', (event) => {
+  let final_position = 250;
+  if (mouse_over.style.width >= 992) {
+    final_position = 50;
+  }
+  let position_y = event.pageY;
+  let position_x = event.pageX;
+  mouse_over.style.top = (position_y - final_position) + "px";
+  mouse_over.style.left = (position_x -final_position) + "px";
+});
+const btn_menu = document.getElementById("menu-btn");
+const header = document.querySelector(".header");
+btn_menu.addEventListener('click', () =>{
+  let actual_class = header.classList[0]
+  let class_header = "header-mobile";
+  if (actual_class == "header-mobile") {
+    class_header = "header-mobile-hidden";
+  }else if (actual_class == "header-mobile-hidden") {
+    class_header = "header-mobile";
+  }
+  console.log(class_header)
+  header.classList.replace(actual_class,class_header);
+  btn_menu.style.animation = "fadeOut 2s ease-out";
+  setTimeout(
+    () => {
+      btn_menu.style.display="none";
+      btn_menu.style.animation = "";
+    }
+  ,1000);
+});
+const header_nav = document.getElementById("header-nav");
+header_nav.addEventListener('click', (e) =>{
+  header.classList.replace("header-mobile", "header-mobile-hidden");
+  btn_menu.style.display="block";
+});
+window.addEventListener("resize" ,()=>{
+  if (window.innerWidth > 1366) {
+    header.classList.replace(header.classList[0], "header");
+  }
+});
+
